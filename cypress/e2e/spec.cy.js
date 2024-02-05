ed209c3294892714364c1ac6e529c9292b9b4108
Ville Suokas <vilsuo1@gmail.com>
2024-02-05 19:11:00 +0200

    added e2e test for single pokemon page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,4 +4,18 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to a pokemon page from the front page', function () {
+    cy.visit('http://localhost:5000')
+
+    const pokemonName = 'ivysaur'
+    cy.get('.list-container')
+      .find(`.list-item .list-item-name:contains(${pokemonName})`)
+      .click()
+
+    cy.url().should('to.match', new RegExp(pokemonName), 'i')
+
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
